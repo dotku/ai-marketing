cc7effc7683d22805ab887dcc94bc15c9ccdc000
Weijing Jay Lin <weijingjaylin@gmail.com>
2025-03-05 16:43:51 -0800

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+inject();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
