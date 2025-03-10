ceadc41e35a6728113dbb325950c3c06849bfb8c
Weijing Jay Lin <weijingjaylin@gmail.com>
2025-03-09 21:30:57 -0700

    update company info

--- src/data/companyInfo.ts
+++ src/data/companyInfo.ts
@@ -1,16 +1,16 @@
-const LEGAL_NAME = "嘿嗨广告传媒有限公司";
+const LEGAL_NAME = "深圳市胜方电子科技有限公司";
 const TITLE = "嘿嗨";
 export const companyInfo = {
   name: TITLE,
   legalName: LEGAL_NAME,
   address: {
     city: "深圳市",
-    district: "南山区",
-    location: "科技园南区高新南七道",
-    full: "深圳市南山区科技园南区高新南七道",
+    district: "福田区",
+    location: "福华一路 98 号卓越大厦 1903",
+    full: "深圳市福田区福华一路 98 号卓越大厦 1903",
   },
   contact: {
-    phone: "17318011997",
+    phone: "0755-26506598",
     email: "carrie_lan198@163.com",
   },
   description: `${LEGAL_NAME}是一家专注于AI驱动的数字营销解决方案提供商，致力于帮助企业实现数字化转型！`,
